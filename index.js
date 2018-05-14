bcccacd99e8b447e5905e8e95ccd203bd140fbbf
Sop <sop.son@feavn.com.vn>
2018-05-14 15:58:53 +0700

    add Pure Chart
    add Pure Chart

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import Picker from './itspicker'
-import SectionMultiSelect from './itssectionmultiselect'
-import ModalDatePicker from './itsdatetimepicker'
-export {Picker,SectionMultiSelect,ModalDatePicker};
+import Picker from "./itspicker";
+import SectionMultiSelect from "./itssectionmultiselect";
+import ModalDatePicker from "./itsdatetimepicker";
+import PureChart from "./pure-chart";
+export { Picker, SectionMultiSelect, ModalDatePicker, PureChart };
---
+++ pure-chart/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { View } from "react-native";
+
+import LineChart from "./components/line-chart";
+import ColumnChart from "./components/column-chart";
+import ColumnChartStack from "./components/column-chart-stack";
+export default class PureChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.renderChart = this.renderChart.bind(this);
+  }
+  renderChart() {
+    if (this.props.type === "line") {
+      return <LineChart {...this.props} />;
+    } else if (this.props.type === "bar") {
+      return <ColumnChart {...this.props} />;
+    } else if (this.props.type === "bar-stack") {
+      return <ColumnChartStack {...this.props} />;
+    } else if (this.props.type === "pie") {
+    }
+  }
+  render() {
+    return <View>{this.renderChart()}</View>;
+  }
+}
+PureChart.propTypes = {
+  type: PropTypes.string.isRequired,
+  data: PropTypes.array.isRequired,
+  color: PropTypes.string,
+  height: PropTypes.number,
+  numberOfYAxisGuideLine: PropTypes.number,
+  customValueRenderer: PropTypes.func,
+  backgroundColor: PropTypes.string
+};
+PureChart.defaultProps = {
+  color: "#297AB1",
+  height: 100,
+  numberOfYAxisGuideLine: 5,
+  backgroundColor: "#FFFFFF"
+};
