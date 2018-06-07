c0d0da6d9d5cefe73d8e20bb311af2b5f32670b7
Sop <sop.son@feavn.com.vn>
2018-06-07 09:44:03 +0700

    add new drawer

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import Picker from "./itspicker";
 import SectionMultiSelect from "./itssectionmultiselect";
 import ModalDatePicker from "./itsdatetimepicker";
 import PureChart from "./pure-chart";
-export { Picker, SectionMultiSelect, ModalDatePicker, PureChart };
+import BouncyDrawer from './react-native-bouncy-drawer'
+export { Picker, SectionMultiSelect, ModalDatePicker, PureChart ,BouncyDrawer };
