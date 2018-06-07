b4f0bc9722057a4b18864a38d7a570a8c85ae57a
Sop <sop.son@feavn.com.vn>
2018-06-07 10:36:08 +0700

    update

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ import Picker from "./itspicker";
 import SectionMultiSelect from "./itssectionmultiselect";
 import ModalDatePicker from "./itsdatetimepicker";
 import PureChart from "./pure-chart";
-import BouncyDrawer from './react-native-bouncy-drawer'
-export { Picker, SectionMultiSelect, ModalDatePicker, PureChart ,BouncyDrawer };
+export { Picker, SectionMultiSelect, ModalDatePicker, PureChart  };
