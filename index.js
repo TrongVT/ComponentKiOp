e8bbb8a8ad8a79378013a94736850eea07bb5503
Sop <sop.son@feavn.com.vn>
2018-06-08 16:05:57 +0700

    add NumberFormat
    add NumberFormat

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import Picker from "./itspicker";
 import SectionMultiSelect from "./itssectionmultiselect";
 import ModalDatePicker from "./itsdatetimepicker";
 import PureChart from "./pure-chart";
-export { Picker, SectionMultiSelect, ModalDatePicker, PureChart  };
+import NumberFormat  from './react-number-format'
+export { Picker, SectionMultiSelect, ModalDatePicker, PureChart,NumberFormat  };
